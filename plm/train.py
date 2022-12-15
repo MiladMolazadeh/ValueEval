1ab0f38d9201f41deb5925fb3aa179e6735756dc
Milad Molazadeh <molazadeh.osku@gmail.com>
2022-12-15 18:49:31 +0330

    [clean]

--- plm/train.py
+++ plm/train.py
@@ -1,14 +1,11 @@
 """
     Training Model
 """
-import torch
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, get_scheduler, AdamW, AutoModel
+from transformers import AutoTokenizer
 from torch.utils.data import DataLoader
-import numpy as np
 # =================== In project packages =====================
 from configuration import BaseConfig
 from data_loader import ValueDataset, load_values_from_json, format_dataset, collate_fn
-from utils import compute_metrics
 from models import ValueClassifier
 
 if __name__ == '__main__':
@@ -29,7 +26,7 @@ if __name__ == '__main__':
     VALID_DL = DataLoader(ValueDataset(VALID_DATA, tokenizer), shuffle=True,
                           collate_fn=collate_fn(),
                           batch_size=_config.batch_size)
-    # TRAIN_DL = [d for dl in [TRAIN_DL, VALID_DL] for d in dl]
+
     num_labels = len(_values['2'])
 
     classifier = ValueClassifier(_config.bert_model, config=_config)
